dc52b0407fe82fa94501b22cf64ec269a632d9dc
Kim Maida <kim.maida@auth0.com>
2018-10-25 15:49:43 -0400

    Update UI for auth error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
-import { map } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,11 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  authError$ = this.auth.authStatus$.pipe(
-    map(event => !!(event === 'login_error'))
+  authError$: Observable<boolean> = this.auth.authStatus$.pipe(
+    filter(event => event === 'login_error' || event === 'login_success'),
+    map(
+      event => !!(event === 'login_error')
+    )
   );
 
   constructor(public auth: AuthService) {}
