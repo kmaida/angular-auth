c98e163745ddaf13fa4b1c26b306c1b509fe3645
Kim Maida <kim.maida@auth0.com>
2018-11-06 17:12:44 -0500

    Add audience

--- api/routes.js
+++ api/routes.js
@@ -26,6 +26,7 @@ module.exports = function(app, config) {
       jwksRequestsPerMinute: 5,
       jwksUri: `https://${config.AUTH0_DOMAIN}/.well-known/jwks.json`
     }),
+    audience: config.AUTH0_CLIENT_ID,
     issuer: `https://${config.AUTH0_DOMAIN}/`,
     algorithm: 'RS256'
   });
